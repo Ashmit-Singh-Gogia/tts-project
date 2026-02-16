91f206c50a56ed6fd2b4fb49e8ab607068b41ccc
Ashmit Singh Gogia <ashmit9955@gmail.com>
2026-02-16 11:59:18 +0530

    feat: integrate gorm and add sqlite database connection

--- backend/main.go
+++ backend/main.go
@@ -1,8 +1,27 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"log"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+type TTSHistory struct {
+	gorm.Model
+	Text string
+}
 
 func main() {
+
+	// github.com/mattn/go-sqlite3
+	db, err := gorm.Open(sqlite.Open("tts.db"), &gorm.Config{})
+	if err != nil {
+		log.Fatal(err.Error())
+		return
+	}
+	db.AutoMigrate(&TTSHistory{})
 	router := gin.Default()
 	router.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
